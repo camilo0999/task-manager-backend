6ea13434eb44853ede9b0debb56f36efc365a893
camilo <camiloibarguen999@gmail.com>
2025-01-07 12:06:01 -0500

    cambio final

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,5 +1,4 @@
 const { Sequelize } = require('sequelize');
-
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
@@ -13,13 +12,21 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 
 sequelize
     .authenticate()
-    .then(() => console.log('Connected to PostgreSQL successfully!'))
+    .then(() => {
+        console.log('Connected to PostgreSQL successfully!');
+        // Sincronizar los modelos con la base de datos
+        return sequelize.sync({ alter: true }); // usar { force: true } para forzar la recreación de tablas
+    })
+    .then(() => console.log('All tables created successfully!'))
     .catch((err) => console.error('Unable to connect to PostgreSQL:', err));
 
 const authenticateDatabase = async () => {
     try {
         await sequelize.authenticate();
         console.log('Conexión a PostgreSQL exitosa');
+        // Sincronizar los modelos con la base de datos
+        await sequelize.sync({ alter: true }); // usar { force: true } para forzar la recreación de tablas
+        console.log('¡Todas las tablas creadas exitosamente!');
     } catch (error) {
         console.error('No se pudo conectar a PostgreSQL:', error);
     }
--- controllers/authController.js
+++ controllers/authController.js
@@ -4,6 +4,12 @@ const User = require('../models/userModel');
 
 exports.register = async (req, res) => {
     const { username, password, role, name, lastname, telefono } = req.body;
+
+    // Verificar que todos los campos están presentes
+    if (!username || !password || !role || !name || !lastname || !telefono) {
+        return res.status(400).json({ error: 'Todos los campos son obligatorios' });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     try {
@@ -15,47 +21,50 @@ exports.register = async (req, res) => {
             name,
             lastname,
             telefono,
-            blockaded: false,
+            bloqueado: false, // Asegúrate de usar la palabra correcta aquí
         });
 
-        res.status(201).json({ message: 'User registered successfully!', user: newUser });
+        res.status(201).json({ message: '¡Usuario registrado exitosamente!', user: newUser });
     } catch (err) {
-        console.error('Error registering user:', err);
-        res.status(500).json({ error: 'Error registering user' });
+        console.error('Error al registrar usuario:', err);
+        res.status(500).json({ error: 'Error al registrar usuario' });
     }
 };
 
-
 exports.login = async (req, res) => {
     const { username, password } = req.body;
+
+    // Verificar que todos los campos estén presentes
+    if (!username || !password) {
+        return res.status(400).json({ error: 'Usuario y contraseña son obligatorios' });
+    }
 
     try {
         const user = await User.findOne({ where: { username } });
 
         if (!user) {
-            return res.status(401).json({ error: 'Invalid credentials' });
+            return res.status(401).json({ error: 'Credenciales inválidas' });
         }
 
         const passwordMatch = await bcrypt.compare(password, user.password);
 
         if (!passwordMatch) {
-            return res.status(401).json({ error: 'Invalid credentials' });
+            return res.status(401).json({ error: 'Credenciales inválidas' });
         }
 
         const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-        res.json({ 
-            "token": token,
-            "username": user.username,
-            "role": user.role,
-            "lastname": user.lastname,
-            "name": user.name,
-            "telefono": user.telefono,
-            "id": user.id,
+        res.json({
+            token,
+            username: user.username,
+            role: user.role,
+            lastname: user.lastname,
+            name: user.name,
+            telefono: user.telefono,
+            id: user.id,
         });
     } catch (err) {
-        console.error('Error logging in user:', err);
-        res.status(500).json({ error: 'Error logging in user' });
+        console.error('Error al iniciar sesión del usuario:', err);
+        res.status(500).json({ error: 'Error al iniciar sesión del usuario' });
     }
-
 };
